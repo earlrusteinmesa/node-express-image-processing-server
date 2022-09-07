97fd645918a0dba20c666879cf7129e01901c0d4
Earl Rustein Mesa <emesa@fullscale.io>
2022-09-07 19:43:01 +0800

    fix issue in Creating the parent thread

--- api/src/imageProcessor.js
+++ api/src/imageProcessor.js
@@ -8,8 +8,8 @@ function imageProcessor(filename) {
   const sourcePath = uploadPathResolver(filename);
   const resizedDestination = uploadPathResolver(`resized-${filename}`);
   const monochromeDestination = uploadPathResolver(`monochrome-${filename}`);
-  const resizeWorkerFinished = false;
-  const monochromeWorkerFinished = false;
+  let resizeWorkerFinished = false;
+  let monochromeWorkerFinished = false;
 
   return new Promise((resolve, reject) => {
     if (isMainThread) {
