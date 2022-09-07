a5dd74e963e5b46ac97a210027481eaa61ca64ee
Earl Rustein Mesa <emesa@fullscale.io>
2022-09-07 18:23:20 +0800

    fix issue in Serving the photo-viewer and creating the workers

--- api/src/resizeWorker.js
+++ api/src/resizeWorker.js
@@ -3,6 +3,9 @@ const { workerData, parentPort } = require("worker_threads");
 
 gm(workerData.source)
   .resize(100, 100)
-  .write(workerData.destination, () => {
+  .write(workerData.destination, (error) => {
+    if (error) {
+      throw error;
+    }
     parentPort.postMessage({ resized: true });
   });
